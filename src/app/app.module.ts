d5217fff13993e9dc50ca352d1e50aace4f96136
Tim Tsai <sky22357168@gmail.com>
2020-09-20 23:25:41 +0800

    Add Report Componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountdownModule } from 'ngx-countdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ReportComponent } from './components/report/report.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
     TranslateModule.forRoot(),
+    TabsModule.forRoot(),
     ModalModule.forRoot(),
     AppRoutingModule,
     CountdownModule 
--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('staticTabs', { static: false }) staticTabs: TabsetComponent;
+  constructor(public bsModalRef: BsModalRef) {}
 
   ngOnInit() {
   }
-
+  selectTab(tabId: number) {
+    this.staticTabs.tabs[tabId].active = true;
+  }
 }
